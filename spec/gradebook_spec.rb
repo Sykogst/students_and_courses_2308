6c7e35e1c577cc4f0e928feed120402bc9c3a879
Sam Tran <samuelttran@gmail.com>
2023-09-06 10:06:14 -0600

    Write test for and add add_course method

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -6,6 +6,12 @@ require './lib/gradebook'
 RSpec.describe Gradebook do
   before(:each) do
     @gradebook = Gradebook.new('Sam')
+    @course1 = Course.new("Calculus", 2)
+    @course2 = Course.new("Biology", 1)
+    @course3 = Course.new("History", 3)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29}) 
+    @student3 = Student.new({name: "Borgan", age: 20})
   end
 
   describe '#initialize' do
@@ -20,7 +26,15 @@ RSpec.describe Gradebook do
   end
 
   describe '#add_course' do
+    it 'Adds courses to gradebook' do
+      expect(@gradebook.courses).to eq([])
+      @gradebook.add_course(@course1)
+      expect(@gradebook.courses).to eq([@course1])
 
+      @gradebook.add_course(@course2)
+      @gradebook.add_course(@course3)
+      expect(@gradebook.courses).to eq([@course1, @course2, @course3])
+    end
   end
 
   describe '#list_all_students' do
