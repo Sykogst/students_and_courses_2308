cab554461dbf2044fa8077aea1c1058bc7af3006
Sam Tran <samuelttran@gmail.com>
2023-09-06 09:47:50 -0600

    Add Course class, and full?/enroll methods to pass spec tests

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,18 @@
 class Course
-  
+  attr_reader :name, :capacity, :students
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @full = false
+  end
+
+  def full?
+    @full
+  end
+
+  def enroll(student)
+    @students << student if @students.length < @capacity
+    @full = true if @students.length == @capacity
+  end
 end
